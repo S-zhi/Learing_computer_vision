440637087a58bd8738eeae608c48918ee98bc7ac
S_zhi <140597239+S-zhi@users.noreply.github.com>
2023-08-19 17:41:34 +0800

    Update 一维投票基础文件_（年龄-身高）.py

--- KNN/引导思想程序/一维投票基础文件.py
+++ KNN/引导思想程序/一维投票基础文件.py
@@ -2,25 +2,26 @@ import numpy
 from sklearn.neighbors import KNeighborsClassifier
 import  numpy as np
 """
-    author : S_zhi   
-    E-mail : feng978744573@163.com
-    project : age --->  high KNN  
-    option : 数据集合的大小，相当于数据库中的测试数据的多少 （20） 
-    进行猜测的值 :  N  >> 第一个输入的值
-    K 值是可以流动的  >>  输入的第二个的值 。  
+    1.author : S_zhi   
+    2.E-mail : feng978744573@163.com
+    3.project :  通过 KNN 来研究 人的年龄对人的身高的影响 
+    4.op : 训练值 ---> createData 年龄 | 身高  
+           训练方法 --->  KNN库中的函数  
 """
 def createData() :
      age = np.array([[1], [4], [7], [9], [10], [12], [15], [18], [20], [24], [30], [40], [50]])
-     high = np.array([1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
+     high = np.array([30, 50, 80, 90, 100,120, 140, 180, 185, 178, 188, 174, 168])
      return age , high
 
 if __name__ == '__main__' :
     age , high = createData()
+    # 再次赋值 
     age_test = int (input("请输入一个想要预测的年龄值"))
     k = int (input("输入一个想要预测的K 值"))
-    knn = KNeighborsClassifier(n_neighbors= k )  # 这里设置K=3，你可以根据需要调整K的值
-    knn.fit(age, high)  # X_train是特征数据，y_train是标签数据
-
+    knn = KNeighborsClassifier(n_neighbors= k ) 
+    # 读入的 K 值 
+    knn.fit(age, high)  
+    # 进行模拟模型
     high_pred = knn.predict(np.array([[age_test]]))
-
+    
     print(high_pred)
