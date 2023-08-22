fa55ad3122152dec12ec0deb90e4909c7be9ebff
S_zhi <140597239+S-zhi@users.noreply.github.com>
2023-08-22 11:59:28 +0800

    Update 大数据生成文件.py

--- KNN/导入大数据文件_第二阶段/大数据生成文件.py
+++ KNN/导入大数据文件_第二阶段/大数据生成文件.py
@@ -9,7 +9,10 @@ data = {
     '孩子身高': []
 }
 
-# 生成100组数据
+"""
+    
+"""
+#随机生成函数 
 for _ in range(100):
     # 随机生成父母平均身高在150到190之间的整数
     parent_height = random.randint(150, 190)
@@ -20,15 +23,23 @@ for _ in range(100):
     # 随机生成孩子性别，1表示男孩，0表示女孩
     child_gender = random.choice([0, 1])
 
-    # 根据父母平均身高、孩子年龄和性别生成孩子身高的简单模拟（仅用于示例，实际情况可能更复杂）
-    if child_age <= 23:
+   
+    if child_age <= 18:
         parent_height -= 50
-        child_height = 50 + parent_height / 20 * child_age + (child_gender * 5)
+        child_height = 40 + parent_height / 16 * child_age + (child_gender * 5)
     else:
         parent_height -= 50
         child_height = 50 + parent_height / 20 * 21 + (child_gender * 5)
     parent_height += 50
-    # 将生成的数据添加到数据字典中
+    """
+    拟合数据 ： 首先是使用分支结构来设置的数据，其实本测试数据不用else : 之后的语句
+    但为了保证数据的合理性，本数据扩展了孩子 18岁之后的身高情况.
+    
+    """
+
+
+    
+    # 将生成的数据添加到数据字典中 相当于 map 字典
     data['父母平均身高'].append(parent_height)
     data['孩子年龄'].append(child_age)
     data['孩子性别'].append(child_gender)
