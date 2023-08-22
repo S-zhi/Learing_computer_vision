0b7c522cb8f97971391efbd33723e2166a57babc
S_zhi <140597239+S-zhi@users.noreply.github.com>
2023-08-22 15:26:29 +0800

    Update 三维拟合.py

--- KNN/导入大数据文件_第二阶段/三维拟合.py
+++ KNN/导入大数据文件_第二阶段/三维拟合.py
@@ -1,7 +1,3 @@
-import numpy
-from sklearn.neighbors import KNeighborsClassifier
-import numpy as np
-
 """ 
     1. author : S_zhi 
     2. E-mail : feng978744573@163.com
@@ -10,21 +6,30 @@ import numpy as np
 """
 
 
+import numpy
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+import pandas as pd
+
+# 创建一个DataFrame 来存储相关的内容   
+data = pd.DataFrame({
+    '父母平均身高': parent_height,
+    '孩子年龄': child_age,
+    '孩子性别': child_gender,
+    '孩子身高': child_height
+})
     # 前面是 年龄 ， 后面是 性别 为 男 的时候 表示为 1 ，性别为女的时候 表示为  0
 def createData():
-    age_man = np.array([[12, 1], [23, 0], [23, 1], [25, 1], [30, 0]])
-    high = np.array([120, 169, 182, 188, 170])
-    return age_man, high
+    loaded_data = pd.read_csv('family_data.csv')
+    父母平均身高 = loaded_data['父母平均身高'].to_numpy()
+    孩子年龄 = loaded_data['孩子年龄'].to_numpy()
+    孩子性别 = loaded_data['孩子性别'].to_numpy()
+    孩子身高 = loaded_data['孩子身高'].to_numpy()
+    return 父母平均身高,孩子年龄,孩子性别,孩子身高
 
 
 if __name__ == '__main__':
-    age_man, high = createData()
-    age_man_test = input("请按这种格式[n,m]来输入年龄和性别：")
-    age_man_test = eval(age_man_test)  # 将输入的字符串转换为列表
+    parent_high , child_age , child_is_man_or_women , child_high = createData()
 
-    k = int(input("请输入K值："))
-    knn = KNeighborsClassifier(n_neighbors=k)
-    knn.fit(age_man, high)
-
-    high_pred = knn.predict([age_man_test])
-    print("预测的身高为:", high_pred[0])
+    #导入创建的数据集合 
+    
