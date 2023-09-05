6efe7c115b616f566b06ef8c54e5f252bfdf311b
S_zhi <140597239+S-zhi@users.noreply.github.com>
2023-09-05 14:51:36 +0800

    Update 单层神经网络引导1.py

--- 神经网络/单层神经网络（感知器）/单层神经网络引导1.py
+++ 神经网络/单层神经网络（感知器）/单层神经网络引导1.py
@@ -4,38 +4,32 @@ import torch.optim as optim
 import numpy as np
 import pandas as pd
 
-""" 
+"""
+    
     1. author : S_zhi 
     2. E-mail : feng978744573@163.com
-    3. project : 基于感知器构建的简单的分类器模型，我们来计算的是
-    
+    3. project : 基于感知器构建的神经网络模型，由于本数据集合比较差劲，所以当前仅仅当作学习神经网络的方法之一。
+    我们只要使用这个项目来学习神经网络的基本的内容就好
+    主要分成下面的几步
+    1，创建模型
+    2，初始化模型（简单的初始化 ， 一些常规函数的定义 ）
+    3，训练模型 
+    4，测试模型
 
 """
 
 
-
-'''
-    第一部分 ： 随机生成一下数据
-    随机生成的一些数据，进行拟合，
-'''
-# 生成一些示例数据
-# 从CSV文件加载数据，假设CSV文件名为data.csv
+# 完成数据的导入
 data = pd.read_excel('data.xlsx')
 
-
-# 提取特征X（假设X包含在所有列中，除了最后一列）
 X = data.iloc[:, :-1].values
 
-# 提取标签y（假设标签y在最后一列）
 y = data.iloc[:, -1].values
 
-
-# 将数据转换为PyTorch张量
 X = X.astype(np.float32)
-  # 使用权重矩阵的数据类型来转换x的数据类型
-X = torch.sigmoid(self.fc(X))
+y = torch.tensor(y, dtype=torch.float32)
+# 完成数据的导入
 
-y = torch.tensor(y)
 
 # 创建一个单层神经网络模型
 class SingleLayerNN(nn.Module):
@@ -52,13 +46,16 @@ model = SingleLayerNN()
 
 # 定义损失函数和优化器
 criterion = nn.BCELoss()  # 二分类交叉熵损失
-optimizer = optim.SGD(model.parameters(), lr=0.1)  # 尝试适度增大学习率
+optimizer = optim.SGD(model.parameters(), lr=0.9)  # 尝试适度增大学习率
+# 初始化模型
+
+
 
 # 训练模型
-num_epochs = 10000
+num_epochs = 10000000
 for epoch in range(num_epochs):
     # 前向传播
-    outputs = model(X)
+    outputs = model(torch.tensor(X, dtype=torch.float32))
     loss = criterion(outputs, y.view(-1, 1))  # 将标签视图重塑为列向量
 
     # 反向传播和优化
@@ -68,6 +65,7 @@ for epoch in range(num_epochs):
 
     if (epoch + 1) % 1000 == 0:
         print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
 
 # 测试模型
 with torch.no_grad():
